9be7f090eafe4a9bdc5d41e2467113712dca08c2
Bishnu Mishra <bishnumishra01996@gmail.com>
2022-06-09 16:09:38 +0530

    power of a number

---
+++ 2_power_no.js
@@ -0,0 +1,11 @@
+//WAP to find the power the number using for loop
+let b, e, i=1,result = 1;
+
+b = 4;
+exponent = 3;
+
+while(i<=exponent){
+    result = result * b;
+    i++;
+}
+console.log(result);
